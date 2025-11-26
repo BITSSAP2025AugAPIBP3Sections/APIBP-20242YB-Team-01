eeeb6924882ef873a78c02408ebf8ab7a2d2da62
Abinav Nuthikatla <110279487+deepikaSingh2711@users.noreply.github.com>
2025-11-26 23:54:21 +0530

    analytics services cleanup

--- services/analytics-service/src/main/java/com/ebaazee/analytics_service/graphql/ReportResolver.java
+++ services/analytics-service/src/main/java/com/ebaazee/analytics_service/graphql/ReportResolver.java
@@ -2,15 +2,12 @@ package com.ebaazee.analytics_service.graphql;
 
 import com.ebaazee.analytics_service.dto.ReportResponse;
 import com.ebaazee.analytics_service.service.ReportService;
+import java.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-
-import java.util.Base64;
-
-// added
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Controller
 public class ReportResolver {
