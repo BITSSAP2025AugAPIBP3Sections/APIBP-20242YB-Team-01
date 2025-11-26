625e094e0453fe90a1512e9b06f0fe3cb3cf897b
Deepika Singh NS <110279487+deepikaSingh2711@users.noreply.github.com>
2025-11-26 23:54:49 +0530

    analytics services cleanup (#32)

--- services/analytics-service/src/main/java/com/ebaazee/analytics_service/security/SecurityConfig.java
+++ services/analytics-service/src/main/java/com/ebaazee/analytics_service/security/SecurityConfig.java
@@ -3,9 +3,9 @@ package com.ebaazee.analytics_service.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -23,7 +23,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthorizationFilter jwtAuthorizationFilter) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthorizationFilter jwtAuthorizationFilter)
+            throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
